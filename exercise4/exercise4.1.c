cf4746fffa47958378d1978e90db13ff2f4fed46
marcospolitecnica <78215406+marcospolitecnica@users.noreply.github.com>
2021-02-11 14:57:02 -0600

    Update exercise4.1.c

--- exercise4/exercise4.1.c
+++ exercise4/exercise4.1.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Name: Marcos\n");
-  printf("Social number: 415423140\n");
-  printf("Date of birth: March, 2nd, 1999\n");
+  printf("Name: Marcos\n"); //print the name
+  printf("Social number: 415423140\n"); //print the social number
+  printf("Date of birth: March, 2nd, 1999\n"); //print the date of birth
   return 0;
 }
