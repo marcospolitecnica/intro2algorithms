58bc9ae40a7a1791eeb6a3754132788050ffada8
marcospolitecnica <78215406+marcospolitecnica@users.noreply.github.com>
2021-02-11 15:33:40 -0600

    Create exercise4.4.c

---
+++ exercise4/exercise4.4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("H   H   EEEEE   L      L        OOO\n"); // prints the first set of letters
+  printf("H   H   E       L      L       O   O\n");// prints the second set of letters
+  printf("H   H   E       L      L       O  OO\n");// prints the third set of letters
+  printf("HHHHH   EEE     L      L       O O O\n");// prints the fourth set of letters
+  printf("H   H   E       L      L       OO  O\n");// prints the fifth set of letters
+  printf("H   H   E       L      L       O   O\n");// prints the sixth set of letters
+  printf("H   H   EEEEE   LLLLL  LLLLL    OOO\n");// prints the seventh set of letters
+  return 0;
+}
