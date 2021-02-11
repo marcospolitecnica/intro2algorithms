353961072daedb1eab16c65c549d8fdd0fe9dc93
marcospolitecnica <78215406+marcospolitecnica@users.noreply.github.com>
2021-02-11 16:18:37 -0600

    Create exercise4.6.c

---
+++ exercise4/exercise4.6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int note; // integer of note
+int note2; //integer of note2
+
+int main(void) {
+  note = 35*35, //multiplication operation 
+  note2 = 10; //statement of 10
+  printf("%d\n", note+note2); //sum of note and note to while in printf
+  return 0;
+}
