aa79a2b5d222c70bdef9541a006ffd6bca993dc9
marcospolitecnica <78215406+marcospolitecnica@users.noreply.github.com>
2021-02-23 18:59:29 -0600

    Create exercise5.6.c

---
+++ chapter5/exercise5.6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+char line[10];
+int minute; //integer of minute
+
+int main(void) {
+  printf("Enter minutes(s): "); //tells the user to input minutes
+  fgets(line, sizeof(line),stdin);
+  sscanf(line, "%d", &minute); //analysis if minutes
+
+  //Here you will see... the easiest method to have the conversion to hours and minutes...
+  
+  printf("%d minutes is: %d hours %d minutes\n", minute, minute/60, minute%60);// Prints the result as hours and minutes (my mind exploded...)
+  
+  return 0;
+}
