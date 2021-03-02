eb189b0c1644ad0e185f9f03896cae21ed1e5160
marcospolitecnica <78215406+marcospolitecnica@users.noreply.github.com>
2021-03-02 11:27:45 -0600

    Update exercise6.2.c

--- chapter6/exercise6.2.c
+++ chapter6/exercise6.2.c
@@ -2,33 +2,33 @@
 #include <math.h>
 
 char line[80];
-int grade;
+int grade; //integer of grade
 
 int main(void){
 
-  printf("What is the grade of the student?: ");
+  printf("What is the grade of the student?: "); //asks the grade of the student
   fgets(line, sizeof(line), stdin);
   sscanf(line, "%d", &grade);
 
   while (1){
 
   if (grade <= 60){
-    printf("Grade is F\n");
+    printf("Grade is F\n"); //prints if grade is below or equal to 60
     break;
   } else if (grade >= 61 && grade <=70){
-    printf("Grade is D\n");
+    printf("Grade is D\n"); //prints if grade is below or equal to 61 or 70
     break;
   } else if (grade >=71 && grade <=80){
-    printf("Grade is C\n");
+    printf("Grade is C\n"); //prints if grade is below or equal to 71 or 80
     break;
   } else if (grade >=81 && grade <=90){
-    printf("Grade is B\n");
+    printf("Grade is B\n"); //prints if grade is below or equal to 81 or 90
     break;
   } else if (grade >=91 && grade <= 100){
-    printf("Grade is A\n");
+    printf("Grade is A\n"); //prints if grade is below or equal to 91 or 100
     break;
   } else{
-    printf("Not a valid grade\n");
+    printf("Not a valid grade\n"); //prints if grade is not valid
     break;
   }
  }
